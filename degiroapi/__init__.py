4424bde6f72e42f06746c3645a4d2320628d1040
Jeroen Overschie <jeroenoverschie@gmail.com>
2022-12-10 16:17:20 +0100

    🔨

--- degiroapi/__init__.py
+++ degiroapi/__init__.py
@@ -77,7 +77,7 @@ class DeGiro:
     def __request(url, cookie=None, payload=None, headers=None, data=None, post_params=None, request_type=__GET_REQUEST,
                   error_message='An error occurred.'):
         headers = {
-            **headers,
+            **(headers or {}),
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
         }
 
